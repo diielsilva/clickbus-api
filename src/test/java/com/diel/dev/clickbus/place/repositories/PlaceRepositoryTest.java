01f17ec097941e31f94585e8f7899ea16fdb9c0c
Daniel Silva <diielsilvasousa@gmail.com>
2024-09-13 11:45:37 -0300

    chore: adding test case for when slug was not found

--- src/test/java/com/diel/dev/clickbus/place/repositories/PlaceRepositoryTest.java
+++ src/test/java/com/diel/dev/clickbus/place/repositories/PlaceRepositoryTest.java
@@ -1,8 +1,7 @@
 package com.diel.dev.clickbus.place.repositories;
 
 import com.diel.dev.clickbus.place.entities.Place;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.MySQLContainer;
@@ -13,12 +12,23 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@DisplayName(value = "Place Repository Test")
 class PlaceRepositoryTest {
 
-    private static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:9.0.1");
+    private static final MySQLContainer<?> MYSQL_CONNECTION = new MySQLContainer<>("mysql:9.0.1");
 
     @Autowired
     private PlaceRepository repository;
+
+    @BeforeAll
+    static void beforeAll() {
+        MYSQL_CONNECTION.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        MYSQL_CONNECTION.close();
+    }
 
     @AfterEach
     void tearDown() {
@@ -26,6 +36,7 @@ class PlaceRepositoryTest {
     }
 
     @Test
+    @DisplayName(value = "A place should be returned, when slug was found")
     void findBySlug_ShouldReturnSlug() {
         Place toCreate = new Place(
                 "Paulista Avenue",
@@ -37,8 +48,16 @@ class PlaceRepositoryTest {
 
         Place created = repository.save(toCreate);
 
-        Optional<Place> retrieved = repository.findBySlug(toCreate.getSlug());
+        Optional<Place> retrieved = repository.findBySlug(created.getSlug());
 
         assertTrue(retrieved.isPresent());
+    }
+
+    @Test
+    @DisplayName(value = "A place should not be returned, when slug was not found")
+    void findBySlug_ShouldNotReturnSlug_WhenSlugIsNotFound() {
+        Optional<Place> retrieved = repository.findBySlug("paulista-avenue");
+
+        assertTrue(retrieved.isEmpty());
     }
 }
