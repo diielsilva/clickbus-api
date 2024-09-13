784b45109a73170611eb7c64a1c5c2667023e1ad
Daniel Silva <diielsilvasousa@gmail.com>
2024-09-13 11:21:29 -0300

    feat: creating place repository

--- src/main/java/com/diel/dev/clickbus/place/repositories/PlaceRepository.java
+++ src/main/java/com/diel/dev/clickbus/place/repositories/PlaceRepository.java
@@ -1,4 +1,12 @@
 package com.diel.dev.clickbus.place.repositories;
 
-public interface PlaceRepository {
+import com.diel.dev.clickbus.place.entities.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaceRepository extends JpaRepository<Place, Integer> {
+    Optional<Place> findBySlug(String slug);
+
 }
--- src/test/java/com/diel/dev/clickbus/place/repositories/PlaceRepositoryTest.java
+++ src/test/java/com/diel/dev/clickbus/place/repositories/PlaceRepositoryTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.diel.dev.clickbus.place.repositories;
+
+import com.diel.dev.clickbus.place.entities.Place;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 class PlaceRepositoryTest {
-  
+
+    private static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:9.0.1");
+
+    @Autowired
+    private PlaceRepository repository;
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void findBySlug_ShouldReturnSlug() {
+        Place toCreate = new Place(
+                "Paulista Avenue",
+                "paulista-avenue",
+                "São Paulo",
+                "SP",
+                OffsetDateTime.now()
+        );
+
+        Place created = repository.save(toCreate);
+
+        Optional<Place> retrieved = repository.findBySlug(toCreate.getSlug());
+
+        assertTrue(retrieved.isPresent());
+    }
 }
