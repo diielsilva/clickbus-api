125e6233b3ede08734dc3f9067b5ce5e92675744
Daniel Silva <diielsilvasousa@gmail.com>
2024-09-13 23:43:42 -0300

    chore: creating entity not found exception and query parameter not specified exception

---
+++ src/main/java/com/diel/dev/clickbus/shared/exceptions/QueryParameterNotSpecifiedException.java
@@ -0,0 +1,7 @@
+package com.diel.dev.clickbus.shared.exceptions;
+
+public class QueryParameterNotSpecifiedException extends RuntimeException {
+    public QueryParameterNotSpecifiedException(String message) {
+        super(message);
+    }
+}
