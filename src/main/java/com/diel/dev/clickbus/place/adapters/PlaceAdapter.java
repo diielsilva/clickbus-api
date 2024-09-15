4d883150f40d06218b53741b0b79aee2a7879d27
Daniel Silva <diielsilvasousa@gmail.com>
2024-09-15 07:30:59 -0300

    chore: creating place adapter to convert dtos

--- src/main/java/com/diel/dev/clickbus/place/adapters/PlaceAdapter.java
+++ src/main/java/com/diel/dev/clickbus/place/adapters/PlaceAdapter.java
@@ -1,4 +1,31 @@
 package com.diel.dev.clickbus.place.adapters;
 
+import com.diel.dev.clickbus.place.dtos.PlaceRequest;
+import com.diel.dev.clickbus.place.dtos.PlaceResponse;
+import com.diel.dev.clickbus.place.entities.Place;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PlaceAdapter {
+
+    public Place toEntity(PlaceRequest body) {
+        return new Place(
+                body.name(),
+                body.city(),
+                body.state()
+        );
+    }
+
+    public PlaceResponse toDTO(Place toSend) {
+        return new PlaceResponse(
+                toSend.getId(),
+                toSend.getName(),
+                toSend.getSlug(),
+                toSend.getCity(),
+                toSend.getState(),
+                toSend.getCreatedAt(),
+                toSend.getUpdatedAt()
+        );
+    }
+
 }
